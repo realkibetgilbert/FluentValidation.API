d17c305e249f97f048ea9dbc243e075144e9cfe0
Gilbert Kibet <kibetgilly354@gmail.com>
2024-05-19 14:57:56 +0300

    Add project files.

---
+++ FluentValidation.API/Program.cs
@@ -0,0 +1,41 @@
+using FluentValidation.API.Validators;
+using FluentValidation;
+using FluentValidation.API.Dtos;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddValidatorsFromAssemblyContaining<UserRegistrationValidator>();
+var app = builder.Build();
+
+app.MapPost("/register", async (UserRegistrationDto request, IValidator<UserRegistrationDto> validator) =>
+{
+    var validationResult = await validator.ValidateAsync(request);
+    if (!validationResult.IsValid)
+    {
+        return Results.ValidationProblem(validationResult.ToDictionary());
+    }
+    // perform actual service call to register the user to the system
+    // _service.RegisterUser(request);
+    return Results.Accepted();
+});
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ FluentValidation.API/Validators/UserRegistrationValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.API.Dtos;
+
+namespace FluentValidation.API.Validators
+{
+    public class UserRegistrationValidator : AbstractValidator<UserRegistrationDto>
+    {
+        public UserRegistrationValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().MinimumLength(4);
+            RuleFor(x => x.LastName).NotEmpty().MaximumLength(10);
+            RuleFor(x => x.Email).EmailAddress().WithMessage("{PropertyName} is invalid ! please check");
+            RuleFor(x=>x.Password).Equal(z=>z.ConfirmPassword).WithMessage("Passwords do not match");
+        }
+    }
+}
